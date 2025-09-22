64311857c4277fa6322821367e9ffcc31f185f16
Guorti <gustavo.ortiz7319@gmail.com>
2025-09-22 13:20:50 -0500

    Test commit

--- app/src/main/java/com/example/keyfairy/feature_calibrate/presentation/YOLO11Segmentation.kt.kt
+++ app/src/main/java/com/example/keyfairy/feature_calibrate/presentation/YOLO11Segmentation.kt.kt
@@ -368,6 +368,7 @@ class YOLO11Segmentation(private val context: Context) {
 
     fun getPianoKeysFromImage(imageBytes: ByteArray?, pianoAreaPercentage: Float): ByteArray? {
 
+
         return try {
 
             val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes!!.size)
